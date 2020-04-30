ea5ccf8159051131e523024509723bac06576f53
rahulk64 <emailrahulk64@gmail.com>
2020-04-30 17:19:10 -0600

    initial code base

---
+++ build/src/CMakeFiles/skinning.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/rahulkrishnan/Documents/GitHub/gfx-final/src/animation_loader_saver.cc" "/Users/rahulkrishnan/Documents/GitHub/gfx-final/build/src/CMakeFiles/skinning.dir/animation_loader_saver.cc.o"
+  "/Users/rahulkrishnan/Documents/GitHub/gfx-final/src/bone_geometry.cc" "/Users/rahulkrishnan/Documents/GitHub/gfx-final/build/src/CMakeFiles/skinning.dir/bone_geometry.cc.o"
+  "/Users/rahulkrishnan/Documents/GitHub/gfx-final/src/gui.cc" "/Users/rahulkrishnan/Documents/GitHub/gfx-final/build/src/CMakeFiles/skinning.dir/gui.cc.o"
+  "/Users/rahulkrishnan/Documents/GitHub/gfx-final/src/main.cc" "/Users/rahulkrishnan/Documents/GitHub/gfx-final/build/src/CMakeFiles/skinning.dir/main.cc.o"
+  "/Users/rahulkrishnan/Documents/GitHub/gfx-final/src/procedure_geometry.cc" "/Users/rahulkrishnan/Documents/GitHub/gfx-final/build/src/CMakeFiles/skinning.dir/procedure_geometry.cc.o"
+  "/Users/rahulkrishnan/Documents/GitHub/gfx-final/src/render_pass.cc" "/Users/rahulkrishnan/Documents/GitHub/gfx-final/build/src/CMakeFiles/skinning.dir/render_pass.cc.o"
+  "/Users/rahulkrishnan/Documents/GitHub/gfx-final/src/shader_uniform.cc" "/Users/rahulkrishnan/Documents/GitHub/gfx-final/build/src/CMakeFiles/skinning.dir/shader_uniform.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLM_ENABLE_EXPERIMENTAL"
+  "GLM_FORCE_RADIANS=1"
+  "GLM_FORCE_SIZE_FUNC=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/local/include"
+  "../lib"
+  "/usr/local/Cellar/glew/2.1.0_1/include"
+  "../lib/utgraphicsutil"
+  "../lib/pmdreader"
+  "../third-party/glm"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/rahulkrishnan/Documents/GitHub/gfx-final/build/CMakeFiles/utgraphicsutil.dir/DependInfo.cmake"
+  "/Users/rahulkrishnan/Documents/GitHub/gfx-final/build/CMakeFiles/pmdreader.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
